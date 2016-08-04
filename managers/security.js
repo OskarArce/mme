69f5622eff6a39e3ab142b0006f27ca1eb080683
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 16:03:54 +0200

    FIX Eror: /access

--- managers/security.js
+++ managers/security.js
@@ -5,7 +5,7 @@ const crypto = require('crypto'),
 	salt = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-pbkdf2',
 	key = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-jwt';
 
-const pbkdf2 = (password) => return new Promise((resolve, reject) => {
+const pbkdf2 = (password) => new Promise((resolve, reject) => {
 	crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
 		if (err) {
 			reject(err);
