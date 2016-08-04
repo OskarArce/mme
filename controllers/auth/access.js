a05e760594a2724e849529fbdc29848ab8900aa5
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 18:20:10 +0200

    FIX Eror: access

--- controllers/auth/access.js
+++ controllers/auth/access.js
@@ -8,13 +8,14 @@ router.post('/', function (req, res) {
 	users.findUser({'nick': req.body.nick, 'password': req.body.password}).then(
 		(user) => {
 			if (user) {
+				res.json({'data': user});
 				securityMgr.token(user).then(
 					(token) => res.json({'data': {'token': token}}),
 					(err) => res.json({'code': 'error_token_auth', 'desc': err})
 				);
 			}
 			else {
-				res.status(404).json({'code': 'error_unauthorized_auth', 'desc': req.body});
+				res.status(404).json({'code': 'error_unauthorized_auth'});
 			}
 		},
 		(err) => res.json({'code': 'error_find_auth', 'desc': err})
