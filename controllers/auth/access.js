a164a8b5ad9233b844a7c6f635e143cc6427bafe
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 15:31:49 +0200

    FIX WTF: /access

--- controllers/auth/access.js
+++ controllers/auth/access.js
@@ -6,7 +6,7 @@ const router = require('express').Router(),
 
 router.post('/', function (req, res) {
 	users.findUser({'nick': req.body.nick, 'password': req.body.password}).then(
-		(user) => res.json({'data': {'token': securityMgr.token(user)}}),
+		(user) => res.json({'data': {'token': 'WTF!'/*securityMgr.token(user)*/}}),
 		(err) => res.json({'code': 'error_listAll_users', 'desc': err})
 	);
 });
