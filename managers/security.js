38e82faecbf4af2c53845bf0e668ef5922e6a2e3
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 15:58:48 +0200

    FIX Eror: /access

--- managers/security.js
+++ managers/security.js
@@ -5,16 +5,14 @@ const crypto = require('crypto'),
 	salt = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-pbkdf2',
 	key = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-jwt';
 
-const pbkdf2 = (password) => {
-	return new Promise((resolve, reject) => {
-		crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
-			if (err) {
-				reject(err);
-			}
-			resolve(key.toString('hex'));
-		});
+const pbkdf2 = (password) => return new Promise((resolve, reject) => {
+	crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
+		if (err) {
+			reject(err);
+		}
+		resolve(key.toString('hex'));
 	});
-};
+});
 
 const token = (user) => new Promise((resolve, reject) => {
 	jwt.sign(user, key, {'expiresIn': 86400}, (err, token) => {
@@ -23,7 +21,7 @@ const token = (user) => new Promise((resolve, reject) => {
 		}
 		resolve(token);
 	});
-};
+});
 
 module.exports = {
 	pbkdf2,
