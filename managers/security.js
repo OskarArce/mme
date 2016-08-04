545a93ea06d68a74c875e3795b61b94cfe11ceb4
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 19:15:07 +0200

    TEST: token2

--- managers/security.js
+++ managers/security.js
@@ -5,23 +5,27 @@ const crypto = require('crypto'),
 	salt = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-pbkdf2',
 	key = 'jIkJ3b3N3chBnIsICVwADMwADMwAjI6ISZtFmTyV2c1JCLxEjOiIXZzVFZpJye-jwt';
 
-const pbkdf2 = (password) => new Promise((resolve, reject) => {
-	crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
-		if (err) {
-			reject(err);
-		}
-		resolve(key.toString('hex'));
-	});
-});
+const pbkdf2 = (password) => {
+	return new Promise((resolve, reject) => {
+		crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(key.toString('hex'));
+		});
+	})
+};
 
-const token = (payload) => new Promise((resolve, reject) => {
-	jwt.sign(payload, key, {'expiresIn': 86400}, (err, token) => {
-		if (err) {
-			reject(err);
-		}
-		resolve(token);
+const token = (payload) => {
+	return new Promise((resolve, reject) => {
+		jwt.sign(payload, key, {'expiresIn': 86400}, (err, token) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(token);
+		});
 	});
-});
+};
 
 module.exports = {
 	pbkdf2,
