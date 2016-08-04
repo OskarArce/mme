24619d4db2e32a9897b9fd62c25ed8e2d820f12f
Oscar Arce <oscar.arce@intelygenz.com>
2016-08-04 19:06:09 +0200

    TEST: token

--- controllers/auth/access.js
+++ controllers/auth/access.js
@@ -5,22 +5,24 @@ const router = require('express').Router(),
 	users = require('../../managers/users');
 
 router.post('/', function (req, res) {
-	users.findUser({'nick': req.body.nick, 'password': req.body.password}).then(
-		(user) => {
-			if (!user) {
-				res.status(404).json({'code': 'error_unauthorized_auth'});
-			}
-			else {
-				let payload = user.toObject({'getters': false, virtuals: false});
-				res.json({'data': {'user': user, 'payload': payload}});
-				// securityMgr.token(payload).then(
-				// 	(token) => res.json({'data': {'token': token}}),
-				// 	(err) => res.json({'code': 'error_token_auth', 'desc': err})
-				// );
-			}
-		},
-		(err) => res.json({'code': 'error_find_auth', 'desc': err})
+	securityMgr.token(req.body).then(
+		(token) => res.json({'data': {'token': token}}),
+		(err) => res.json({'code': 'error_token_auth', 'desc': err})
 	);
+	// users.findUser({'nick': req.body.nick, 'password': req.body.password}).then(
+	// 	(user) => {
+	// 		if (!user) {
+	// 			res.status(404).json({'code': 'error_unauthorized_auth'});
+	// 		}
+	// 		else {
+	// 			securityMgr.token(payload).then(
+	// 				(token) => res.json({'data': {'token': token}}),
+	// 				(err) => res.json({'code': 'error_token_auth', 'desc': err})
+	// 			);
+	// 		}
+	// 	},
+	// 	(err) => res.json({'code': 'error_find_auth', 'desc': err})
+	// );
 });
 
 module.exports = router;
